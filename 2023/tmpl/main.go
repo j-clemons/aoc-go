ceb6caa6d3c74972cf844555723d44faef743727
Jonathan <j-clemons@tuta.io>
2023-12-05 22:18:15 -0600

    add execution timing to tmpl

--- 2023/tmpl/main.go
+++ 2023/tmpl/main.go
@@ -4,9 +4,18 @@ import (
     "fmt"
     "log"
     "os"
+    "time"
     "strings"
     "strconv"
 )
+
+func track(msg string) (string, time.Time) {
+	return msg, time.Now()
+}
+
+func duration(msg string, start time.Time) {
+	log.Printf("%v: %v\n", msg, time.Since(start))
+}
 
 func toInt(str string) int {
     i, err := strconv.Atoi(str)
@@ -18,6 +27,7 @@ func toInt(str string) int {
 }
 
 func promptFunc(input string) int {
+    defer duration(track("promptFunc"))
     input_lines := strings.Split(input, "\n")
 
     return nil
